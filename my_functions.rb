6b50983a99c92a54c8a973ef4071526400466bac
Fidelma <fidelma.beagan@gmail.com>
2019-06-26 14:25:18 +0100

    lab completed

---
+++ my_functions.rb
@@ -0,0 +1,24 @@
+def add_array_lengths(array1,array2)
+  return array1.length + array2.length
+end
+
+def sum_array(numbers)
+  total = 0
+  for number in numbers
+    total += number
+  end
+  return total
+end
+
+def find_item(array, item)
+  for name in array
+    if name == item
+      return true
+    end
+  end
+  return false
+end
+
+def get_first_key(hash)
+  return hash.keys[0]
+end
